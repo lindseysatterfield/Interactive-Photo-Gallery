e8f3a3263922cee3037b9e44dab859dfcadde943
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2019-02-10 18:32:13 -0600

    Initial commit
    Added HTML, CSS, and Lightbox functionality

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Interactive-Photo-Gallery
+# Interactive Photo Gallery
+
+#### With this project, I created an interactive photo gallery using JavaScript and jQuery. After writing the HTML, I wrote my CSS styles in Sass to make the site responsive and mobile friendly with a media query `mixin`. When the user clicks on a thumbnail, the photo will display in a lightbox. Image slideshow was created using a jQuery plugin, Lightbox by Lokesh Dhakar. The search area has a filter feature made possible by JavaScript. Photos will show and hide depending on user input.
---
+++ js/app.js
@@ -0,0 +1,13 @@
+lightbox.option({
+  'wrapAround': false
+});
+
+
+
+// First try to get the value of the search field whenever a user types in that field, and log that value to the screen with a console.log() statement. Checkout the​ k​eyup event listener​ for this. And be sure to make the value case insensitive by using something like the​ t​oLowerCase()​ m​ethod.
+
+//Next, start trying to target the entire caption by using the​ g​etAttribute()​ m​ethod, then looping​ ​and logging the all lowercase version of the captions to the console.
+
+//Next comes the fun part. Start trying to find ways to check if the current value of the search inputs i​ncluded​ w​thin any of the captions, and if so,log the associated image to the console.
+
+//Lastly, use a​ c​onditional​ ​so that if there's a match,​ d​isplay​ t​he container of the image, and if not, hide it​.​
